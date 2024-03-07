627bd1c30e780cae28d22408f01c546128aceebf
bhaskar joshi <159981822+joshibhaskar684@users.noreply.github.com>
2024-03-07 21:05:34 +0530

    loginpage

---
+++ scriptt.js
@@ -0,0 +1,11 @@
+const signin8tnlink = document.querySelector(".signin8tn-link");
+const signup8tnlink = document.querySelector(".signup8tn-link");
+const wrapper = document.querySelector(".wrapper");
+signup8tnlink.addEventListener('click',() => {
+    wrapper.classList.toggle('active');
+});
+signin8tnlink.addEventListener('click',() => {
+    wrapper.classList.toggle('active');
+});
+
+
